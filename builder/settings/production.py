eda3e25621281e58acf80ec3bae52747e95db6f5
brown-cookies <jesimielalmedejar1018@gmail.com>
2025-03-27 22:58:20 +0800

    add all origin

--- builder/settings/production.py
+++ builder/settings/production.py
@@ -8,8 +8,8 @@ ALLOWED_HOST = ['*']
 # TODO: Update cors configuration before deployment
 
 # Cors header configuration
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = []
 
-CORS_ALLOWED_ORIGIN_REGEXES = ['*']
+CORS_ALLOWED_ORIGIN_REGEXES = []
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
